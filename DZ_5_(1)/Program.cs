a7b2f5b598051f1ce080433cb73499d1a5034bce
Kristina <kristinka.antipova94@gmail.com>
2021-11-16 19:34:43 +0500

    DZ

---
+++ DZ_5_(1)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace DZ_5__1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "текстовый файл.txt";
+            File.WriteAllText(filename, " Произвольный набор данных");
+        }
+    }
+}
---
+++ DZ_5_(3)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+
+namespace DZ_5__3_
+{
+    [Serializable]
+    class Program
+    {
+
+        static void Main()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            var array = new byte[] { 1, 2, 3 };
+            for (byte i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            FileStream fs = new FileStream("File.bin", FileMode.OpenOrCreate);
+            try
+            {
+                formatter.Serialize(fs, array);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+    }
+
+}
+
+
